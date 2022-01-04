d577b6bc34da4806ece9019f989dae9b084d30b2
Phillip <Flip.dlankford@gmail.com>
2022-01-04 09:18:55 -0800

    added books route

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// routes 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
+
+// require
 const db = require('./models');
 
 const app = express();
@@ -23,6 +27,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 (async () => {
 
---
+++ routes/books.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Book = require("../models").Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET home page. */
+router.get('/', asyncHandler( async (req, res, next) => {
+  const books = await Book.findAll();
+  res.render('layout');
+}));
+
+module.exports = router;
